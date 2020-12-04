d6c60570ee839915fbb20d9d933b99c74f3c0e1d
nascimentoJulio <nascimentoJulio@gmail.com>
2020-12-03 23:18:07 -0200

    Criação do projeto

--- EnglishMachine/app/src/main/java/com/smpua/magodev/englishmachine/MainActivity.java
+++ EnglishMachine/app/src/main/java/com/smpua/magodev/englishmachine/MainActivity.java
@@ -1,14 +1,41 @@
 package com.smpua.magodev.englishmachine;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
+
 public class MainActivity extends AppCompatActivity {
+
+    private SmartTabLayout mSmartLayout;
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.mSmartLayout = findViewById(R.id.smart_tab_layout);
+        this.mViewPager = findViewById(R.id.view_pager);
+
+        if (getSupportActionBar()!=null){
+            getSupportActionBar().setElevation(0);
+        }
+
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(getSupportFragmentManager(),
+                FragmentPagerItems.with(this)
+                        .add("Animais",AnimaisFragment.class)
+                        .add("Numeros", NumerosFragment.class)
+                        .add("Vogais", VogaisFragment.class)
+                        .create()
+        );
+
+        mViewPager.setAdapter(adapter);
+        mSmartLayout.setViewPager(mViewPager);
+
     }
 }
